c2dfd65e1a22c926df7da4987c9366c4aa450eb7
Cosmicpearl <cosmicpearl@yonsei.ac.kr>
2020-10-10 17:05:06 +0900

    dabc

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Eugene_Kim
 {
@@ -6,7 +7,53 @@ namespace Eugene_Kim
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x = 3;
+            x = int.Parse(Console.ReadLine());
+            x +=1;
+            Console.WriteLine(x);
+
+            string[] monsters = {"아이린", "슬기"};
+            int[] monsterLevel = new int[2];
+            monsterLevel[0] = 2;
+            monsterLevel[1] = 3;
+
+            List<string> items = new List<string>();
+            items.Add("생명물약30");
+            items.Add("마법물약30");
+
+            items.RemoveAt(0);
+
+            Console.WriteLine("가지고 있는 아이템");
+            Console.WriteLine(items[0]);
+
+            Console.WriteLine("Eu" + "gene");
+
+            int health = 30;
+            int mana = 25;
+            bool isBadCondition = health <= 50 && mana <=20;
+            string condition = isBadCondition ? "나쁨" : "좋음"; //if == True : False
+
+            switch (monsters[0]) 
+            {
+                case "아이린":
+                case "슬기":
+                    Console.WriteLine("레드벨벳 출현");
+                break;
+                default:
+                    Console.WriteLine("???? 출현");
+                    break;
+            }
+
+
+            Person a = new Person();
+            a.age = 26;
+            a.name = "슬기";
+            a.weapon = "vocal";
+            a.print();
+
+            private static int a();
+
+
         }
     }
 }
